bad0d73fae81297e1513d96270f1284566b9e64a
tomasrife <tomas.rife@gmail.com>
2025-12-16 11:17:34 +0100

    edit

--- codis R/r_grafica_vent.R
+++ codis R/r_grafica_vent.R
@@ -1,28 +1,23 @@
-# ============================================================
-# 1) PREPARACIÓ DE DADES
-# ============================================================
 sarapita <- read.csv("error_sarapita.csv", header = TRUE)
 sarapita$time <- as.POSIXct(sarapita$time, format="%Y-%m-%d %H:%M:%S")
 
 dosrius <- read.csv("error_dosrius.csv", header = TRUE)
 dosrius$time <- as.POSIXct(dosrius$time, format="%Y-%m-%d %H:%M:%S")
 
-# ############################################################
-# 2) CONFIGURACIÓ DEL PANELL
-# ############################################################
-par(mfrow = c(2, 1), mar = c(4, 5, 3, 1)) # Marge esquerre (5) una mica més ample per les lletres
+
+par(mfrow = c(2, 1), mar = c(4, 5, 3, 1)) 
 
 # ############################################################
 # GRÀFIC 1: Direcció Vent SA RÀPITA
 # ############################################################
 plot(x = sarapita$time, 
      y = sarapita$dir, 
-     type = "p",           # Punts
-     pch = 19,             # Cercle sòlid
-     col = "purple",       # Color lila
+     type = "p",           
+     pch = 19,             
+     col = "purple",       
      cex = 1,
-     ylim = c(0, 360),     # Els graus van de 0 a 360 obligatòriament
-     yaxt = "n",           # "n" vol dir: "No dibuixis l'eix Y automàtic, que el faré jo"
+     ylim = c(0, 360),     
+     yaxt = "n",          
      main = "Direcció del Vent: Sa Ràpita",
      xlab = "",           
      ylab = "Direcció")
@@ -33,7 +28,7 @@ axis(2, at = c(0, 90, 180, 270, 360),
 
 
 abline(h = c(90, 180, 270), col = "lightgray", lty = 3)
-grid(nx = NULL, ny = NA) # Només graella vertical
+grid(nx = NULL, ny = NA) 
 
 # ############################################################
 # GRÀFIC 2: Direcció Vent DOSRIUS
@@ -42,10 +37,10 @@ plot(x = dosrius$time,
      y = dosrius$dir, 
      type = "p", 
      pch = 19, 
-     col = "darkcyan",     # Color blau-verdaci
+     col = "darkcyan",     
      cex = 1,
      ylim = c(0, 360),
-     yaxt = "n",           # Amaguem eix automàtic
+     yaxt = "n",           
      main = "Direcció del Vent: Dosrius",
      xlab = "Data i Hora",
      ylab = "Direcció")
@@ -57,3 +52,4 @@ axis(2, at = c(0, 90, 180, 270, 360),
 
 abline(h = c(90, 180, 270), col = "lightgray", lty = 3)
 grid(nx = NULL, ny = NA)
+
